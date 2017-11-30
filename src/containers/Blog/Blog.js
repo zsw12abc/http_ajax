2aeb6f341e4521fbc145bd9ff14b1f234a2f5832
Shaowei <zsw22@vip.qq.com>
2017-11-30 20:41:45 +1100

    use link to switch page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import './Blog.css';
 
 class Blog extends Component {
@@ -12,8 +12,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=ture'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
