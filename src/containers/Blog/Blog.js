767f686d5bee626866c89d0a0d96e8b87c70eb8a
Shaowei <zsw22@vip.qq.com>
2017-11-30 11:57:04 +1100

    handling error locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostedId: null
+        selectedPostedId: null,
+        error: false
     };
 
     componentDidMount() {
@@ -23,6 +24,9 @@ class Blog extends Component {
                 }
             });
             this.setState({posts: updatedPosts});
+        }).catch(error => {
+            console.log(error);
+            this.setState({error: true})
         });
     }
 
@@ -31,12 +35,15 @@ class Blog extends Component {
     };
 
     render() {
-        const posts = this.state.posts.map(
-            post => {
-                return <Post key={post.id} title={post.title} author={post.author}
-                             clicked={() => this.postSelectedHandler(post.id)}/>
-            }
-        );
+        let posts = <p style={{textAlign: 'center'}}> Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(
+                post => {
+                    return <Post key={post.id} title={post.title} author={post.author}
+                                 clicked={() => this.postSelectedHandler(post.id)}/>
+                }
+            );
+        }
         return (
             <div>
                 <section className="Posts">
