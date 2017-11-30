d6b1214dec88fa08caabefc58926014566d121d0
Shaowei <zsw22@vip.qq.com>
2017-11-30 20:37:00 +1100

    switching bewteen pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import Posts from './Posts/Posts'；
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import {Route} from 'react-router-dom'
 import './Blog.css';
 
 class Blog extends Component {
@@ -15,7 +17,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                {/*<Route path="/" exact render={() => <h1>home</h1>}/>*/}
+                {/*<Route path="/" render={() => <h1>home2</h1>}/>*/}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post"  component={NewPost}/>
             </div>
         );
     }
