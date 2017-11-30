ba9b9328edc8c15974b1670bb052e1e903a751cf
Shaowei <zsw22@vip.qq.com>
2017-11-30 11:11:31 +1100

    transform the data and add information into data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,14 +14,21 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
             // console.log(response);
-            this.setState({posts: response.data});
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Shaowei'
+                }
+            });
+            this.setState({posts: updatedPosts});
         });
     }
 
     render() {
         const posts = this.state.posts.map(
             post => {
-                return <Post key={post.id} title={post.title}/>
+                return <Post key={post.id} title={post.title} author={post.author}/>
             }
         );
         return (
