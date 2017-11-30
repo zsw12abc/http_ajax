91a7ed617c5ca9e70b35276b1092159665b33ee8
Shaowei <zsw22@vip.qq.com>
2017-11-30 23:17:21 +1100

    using Switch to only render one link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
@@ -31,9 +31,12 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>home</h1>}/>*/}
                 {/*<Route path="/" render={() => <h1>home2</h1>}/>*/}
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/:id" exact component={FullPost}/>
+                {/*Using Switch to only render the first find link*/}
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }
