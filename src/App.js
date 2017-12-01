acc3ed991e1fd040c65168a982d03203376c99c1
Shaowei <zsw22@vip.qq.com>
2017-12-01 18:22:35 +1100

    add asyn component to avoid loading all the content at the beginning

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ class App extends Component {
     render() {
         return (
             //add router to all the components in website.
+            //set the base url for the website
+            /*<BrowserRouter basename={'/'}>*/
             <BrowserRouter>
                 <div className="App">
                     <Blog/>
                 </div>
             </BrowserRouter>
-        );
+        )
     }
 }
 
